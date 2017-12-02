0976882b104583c835a22cb512a528cd32f62b26
Adrianacmy <adrianamullaney@gmail.com>
2017-12-02 11:40:16 -0800

    create location model

--- models/location.py
+++ models/location.py
@@ -1,2 +1,17 @@
 from app import db
 
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.string(50), unique=True)
+    lat = db.Column(db.Float)
+    lon = db.Column(db.Float)
+    safe_score = db.Column(db.Integer, default=0)
+
+def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+def __repr__(self):
+    return 'name: {} '.format(self.name)
