1801fd6901e65ca8bb0369d55361a93e4518863b
razzlepdx <rwilson.or@gmail.com>
2017-12-02 01:30:09 -0800

    add app.py, requirements file, and 2 routes with templates

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, redirect, render_template, flash, url_for, json, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_googlemaps import GoogleMaps
+import os
+
+# app and db dev environment settings:
+######################################
+db_uri = 'mysql+pymysql://sleep-safe:sleep-safe@localhost:8889/sleep-safe'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'ZAj08N/$3m]XHjHy!rX R/~?X,9RW@UL'
+
+db = SQLAlchemy(app)
+
+# routes:
+#########
+
+# Sleep Safe landing page - accessed at localhost:5000 for now
+@app.route("/", methods=['GET'])
+def index():
+    ''' displays the Sleep Safe landing page '''
+    return render_template('index.html')
+
+@app.route("/site", methods=['GET'])
+def location_detail():
+    ''' displays the location detail page about the site selected by the user '''
+    return render_template('location.html')
+
+# runs the app, always the last line
+if __name__ == '__main__':
+    app.run(threaded = True)
