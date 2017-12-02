6d0183b5df1391ce786138f8f011fbd87d74de36
Stephanie Gurung <stephr3@gmail.com>
2017-12-02 12:43:46 -0800

    add functioning google map

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, redirect, render_template, flash, url_for, json, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_googlemaps import GoogleMaps
-from models.user import User
+from flask_googlemaps import Map
 import os
+from keys import *
 
 # app and db dev environment settings:
 ######################################
@@ -14,6 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ZAj08N/$3m]XHjHy!rX R/~?X,9RW@UL'
 
 db = SQLAlchemy(app)
+GoogleMaps(app, key=keys())
 
 # routes:
 # all routes get a decorator `@app.route` that takes at least one parameter - the url.  
@@ -23,8 +25,31 @@ db = SQLAlchemy(app)
 # Sleep Safe landing page - accessed at localhost:5000 for now
 @app.route("/", methods=['GET'])
 def index():
-    ''' displays the Sleep Safe landing page '''
-    return render_template('index.html')
+    ''' displays the Sleep Safe map '''
+    mymap = Map(
+        identifier="mymap",
+        lat=45.5435634,
+        lng=-122.674997,
+        markers=[
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+             'lat': 45.5435634,
+             'lng': -122.674997,
+             'infobox': "<b>Puppet</b>"
+          },
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+             'lat': 45.4724105,
+             'lng': -122.6648244,
+             'infobox': "<b>Oaks Park</b>"
+          }
+        ],
+        zoom=10,
+        maptype_control=False,
+        streetview_control=False,
+        fullscreen_control=False
+    )  
+    return render_template('index.html', mymap=mymap)
 
 @app.route("/site", methods=['GET'])
 def location_detail():
